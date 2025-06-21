e4b8c69a8a24ecf692062678d151f6870b2bfc95
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-06-22 01:05:33 +0600

    I have complete 4 routes

--- src/app/controllers/bookController.ts
+++ src/app/controllers/bookController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Book from "../models/bookModel";
 
+// create book -> api/books
+
 export const createBook = async (req: Request, res: Response) => {
    try {
       const book = new Book(req.body);
@@ -15,6 +17,91 @@ export const createBook = async (req: Request, res: Response) => {
       res.status(500).json({
          success: false,
          message: "Failed to create book!",
+      })
+   }
+}
+
+
+// get all books -> api/books
+
+export const getBooks = async (req: Request, res: Response) => {
+   try {
+
+      const { filter, sortBy, sort, limit } = req.query;
+
+      const valueOfLimit = limit ? parseInt(limit as string) : 10;
+      const sortField = (sortBy as string) || 'createdAt';
+      const sortOrder = sort === 'desc' ? -1 : 1;
+
+      const query: any = {};
+      if (filter) {
+         query.genre = (filter as string).toUpperCase();
+      }
+
+
+      const data = await Book.find(query).sort({ [sortField]: sortOrder }).limit(valueOfLimit)
+      res.status(200).send({
+         success: true,
+         message: 'Books retrieved successfully',
+         data
+      })
+   } catch (error) {
+      res.status(500).json({
+         success: false,
+         message: 'Failed to fetch the books'
+      })
+   }
+}
+
+
+// get single book -> api/books/:bookId
+
+export const getSingleBook = async (req: Request, res: Response) => {
+   try {
+      const bookId = req.params.bookId;
+      const data = await Book.findById(bookId);
+      res.status(200).send({
+         success: true,
+         message: 'Book retrieved successfully',
+         data
+      })
+
+   } catch (error) {
+      res.status(500).json({
+         success: false,
+         message: 'Failed to fetch the books'
+      })
+   }
+
+}
+
+
+// update a book -> /api/books/:bookId
+
+export const updateBook = async (req: Request, res: Response) => {
+   try {
+      const bookId = req.params.bookId;
+      const updatedDoc = req.body || {};
+
+      if(!updatedDoc){
+         res.status(404).json({
+            success: false,
+            message: "Book data is missing!"
+         })
+      }
+
+      const doc = await Book.findByIdAndUpdate(bookId, updatedDoc, { new: true })
+
+
+      res.status(200).send({
+         success: true,
+         message: 'Book updated successfully',
+         data: doc
+      })
+   } catch (error) {
+      res.status(500).json({
+         success: false,
+         message: 'Failed to update book'
       })
    }
 }
--- src/app/routes/bookRoute.ts
+++ src/app/routes/bookRoute.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createBook } from "../controllers/bookController";
+import { createBook, getBooks, getSingleBook, updateBook } from "../controllers/bookController";
 
 const bookRouter = Router();
 
 
-bookRouter.post('/', createBook)
+bookRouter.post('/', createBook);
+bookRouter.get('/', getBooks);
+bookRouter.get('/:bookId', getSingleBook)
+bookRouter.put('/:bookId', updateBook)
 
 export default bookRouter;
